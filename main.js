24ee654a4d598142374888cb9aee6e3cbe92b1fd
Higor Oliveira <higor.oliveira@grupomult.com.br>
2020-04-01 15:43:09 -0300

    initial commit

---
+++ main.js
@@ -0,0 +1,19 @@
+const { resolve } = require("path");
+const { app, Menu, Tray } = require("electron");
+
+// app.dock.hide();
+
+app.on("ready", () => {
+  const tray = new Tray(resolve(__dirname, "assets", "tray-icon.png"));
+
+  const contextMenu = Menu.buildFromTemplate([
+    {
+      label: "Item1",
+      type: "radio",
+      checked: true
+    }
+  ]);
+
+  tray.setToolTip("My Application");
+  tray.setContextMenu(contextMenu);
+});
